176f0cc90b69d7d77b08be0d3795ccd7d5ed404c
Kanav Dhanda <70010606+kanavdhanda@users.noreply.github.com>
2024-04-11 00:50:51 +0530

    lol

--- Frontend/js/profile.js
+++ Frontend/js/profile.js
@@ -8,16 +8,18 @@ $(document).ready(function(){
     $('#roomNumber').text('Your Room Number');
 
     // Current Status Start
-    var currentStatus = 'absent'; /* Change current status value here  it will effect all*/
+    var currentStatus = 'present'; /* Change current status value here  it will effect all*/
     $('#currentStatus').text(currentStatus);
     $('#currentStatusBox').addClass(currentStatus);
      
 
-    $('#reliefTime').on('input', function() {
-        $('#output').text($(this).val() + ' mins');
-    });
+    // $('#reliefTime').on('input', function() {
+    //     $('#output').text($(this).val() + ' mins');
+    // });
 });
-
+document.getElementById('reliefTime').addEventListener('input', function() {
+    document.getElementById('output').textContent = this.value + ' mins';
+});
 
 // document.getElementById("reliefTime").stepUp(10);
 //trying to make it so that the slider increases with increments of 10
